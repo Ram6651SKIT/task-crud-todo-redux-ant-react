4afa5152165f06d96134210436a8c4fc024543bf
Ram6651SKIT <33284645+Ram6651SKIT@users.noreply.github.com>
2020-06-14 17:55:11 +0530

    testing

---
+++ src/components/forms/todoForm.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {Form} from "@ant-design/compatible";
+import {  DatePicker,Input } from "antd";
+
+export const TodoCreateForm = Form.create({ name: "form_in_modal" })(
+  class extends React.Component {
+    render() {
+      const { form, handleSubmit } = this.props;
+      const { getFieldDecorator } = form;
+
+      const formItemLayout = {
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 8 }
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 16 }
+        }
+      };
+      const config = {
+        rules: [
+          { type: "object", required: true, message: "Please select time!" }
+        ]
+      };
+
+      return (
+        <Form {...formItemLayout} onSubmit={handleSubmit}>
+          <Form.Item label="Action">
+            {getFieldDecorator("action")(<Input placeholder="Action" />)}
+          </Form.Item>
+          <Form.Item label="Date">
+            {getFieldDecorator("dateadded", config)(
+              <DatePicker showTime format="YYYY-MM-DD HH:mm:ss" />
+            )}
+          </Form.Item>
+        </Form>
+      );
+    }
+  }
+);
+
